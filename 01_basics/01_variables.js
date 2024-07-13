1fa96526003519c0174b65fdd2df854937c8c215
Joydeeeep <joydeep3debsinha@gmail.com>
2024-07-14 00:29:26 +0530

    comment edit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const a = 1234
 let b = "abcd"
 var c = "xyz"
-d = "VSKP"
+d = "VSKP" // not a good practice to declare a variable  this way
 
 // do not prefer var due to problems in local scope and functional scope
 
